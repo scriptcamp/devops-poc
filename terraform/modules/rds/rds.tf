caa2a4982cbdc28622ff88e5093f5ed4c0998731
manoj-md <86720825+manoj-md@users.noreply.github.com>
2021-07-04 17:54:23 +0530

    Create rds.tf

---
+++ terraform/modules/rds/rds.tf
@@ -0,0 +1,20 @@
+resource "aws_db_instance" "default" {
+  identifier           = var.identifier 
+  allocated_storage    = var.db_storage
+  engine               = var.db_engine
+  engine_version       = var.db_engine_version 
+  instance_class       = var.db_instance_class
+  name                 = var.db_name
+  username             = var.db_username
+  password             = var.db_password
+  parameter_group_name = var.db_parameter_group_name
+  skip_final_snapshot  = var.db_skip_final_snapshot
+
+  vpc_security_group_ids = [var.vpc_security_group_ids[0]]
+  tags = {
+   Service = "rds"
+   Env     = "prod"
+   Application = "scriptcamp"
+   Service-name = "scriptcamp_db"
+  }
+}
